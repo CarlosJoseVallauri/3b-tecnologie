1b88f431561529d6f16113845e5bc9d829d763c3
carlosjoseisnan <carloriso2008@gmail.com>
2025-04-13 19:11:59 +0200

    Added comments

--- NODE/Moduli/src/commonjsmodule.js
+++ NODE/Moduli/src/commonjsmodule.js
@@ -7,3 +7,6 @@ function sayHello(name){
 }
 
 module.exports.greet = sayHello; // Funzione pubblica con alias
+//exports.greet = sayHello; // Uso dell'alias exports
+//module.exports = sayHello; // Esporto un'unica funzione
+//exports = sayHello; // Errore, exports è un riferimento e non può essere modificato
