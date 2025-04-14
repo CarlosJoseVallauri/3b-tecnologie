c8a250f9851b4802c5f659068c88349fe6abb8cf
Carlo <carloriso2008@gmail.com>
2025-04-14 08:58:48 +0200

    Added headers and timeout handling

--- NODE/HTTP/src/app.js
+++ NODE/HTTP/src/app.js
@@ -4,26 +4,41 @@ const os = require("node:os");
 const fs = require("node:fs");
 const server = http.createServer((req, res) => {
     if(req.url === '/'){ // Se la connessione richiede la home, mostra 'Hello world!'
-        res.write("Hello world!");
+        res.writeHead(200, { // Scrive gli header per contenuto e connessione, riportando anche lo status code
+            'Content-Type': 'text/plain', // Definisce il tipo di dati scritti
+            'Connection': 'keep-alive' // Definisce il tipo di connessione
+        });
+        res.write("Hello world!"); // Anche res.end("Hello world!"); per semplificare
         res.end();
     }
     else if(req.url === '/studenti'){
         fs.readFile("./studenti.json", "utf-8", (err, file) => {
             if(err){
                 console.error(err);
-                res.statusCode = 500; // Server error
+                //res.statusCode = 500; // Server error
+                res.writeHead(500, {
+                    'Content-Type': 'text/plain', 
+                    'Connection': 'keep-alive' 
+                });
                 res.write("Internal Server Error");
                 res.end();
             }
             else{
+                res.writeHead(200, {
+                    'Content-Type': 'application/json',
+                    'Connection': 'keep-alive'
+                });
                 res.write(file); // Scrive un file JSON in formato stringa
                 res.end();
             }
         });
     }
     else{ // Altrimenti, restituisci 404
-        res.statusCode = 404;
-        res.write("Error 404, non trovato");
+        res.writeHead(404, {
+            "Content-Type": "text/plain",
+            "Connection": "keep-alive"
+        });
+        res.write(`Couldn't find resource ${req.url}`);
         res.end();
     }
 }); // Crea un server con le stesse funzionalità (e più) di EventEmitter
@@ -46,6 +61,7 @@ server.on("connection", (socket) => {
     });
 });
 
-
+server.timeout = 0; // Numero di millisecondi dopo i quali il server assume che il socket sia andato in timeout
+// server.maxConnections = 1; // Numero massimo di connessioni al server
 server.listen(3000);  // Sceglie la porta su cui ascoltare
 console.info("Listening on port 3000");
